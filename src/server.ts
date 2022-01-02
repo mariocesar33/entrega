410a1384627e3476e2803e5486e9ddf03a97e3b7
Mário César Silva <mcgato33@hotmail.com>
2022-01-02 09:18:09 -0100

    update

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,4 @@ app.get("/", (request, response) => {
   return response.json({message: "hello world!"});
 });
 
-app.listen(3333, () => console.log("Server start, on port 3333!"));
+app.listen(3333, () => console.log("Server start 😀, on port 3333!"));
