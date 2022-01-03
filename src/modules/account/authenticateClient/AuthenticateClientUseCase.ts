5aa633495a9b7ddfbc0b36671074f0d66c2a181e
Mário César Silva <mcgato33@hotmail.com>
2022-01-03 05:45:34 -0100

    Create AuthenticateClientUseCase.ts

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,12 @@
+export class AuthenticateClientUseCase {
+  async execute(){
+    // Receber username e password
+
+    // Verificar se username cadastrado
+
+    // verificar se password esta correto
+
+    // Gerar o token
+
+  }
+}
