2cfca1f6cd80ee1ae571ec1897c641bfe8974843
Mário César Silva <mcgato33@hotmail.com>
2022-01-03 05:13:32 -0100

    importar arquivo routes para dentro de server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
+import { routes } from "./routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({message: "hello world!"});
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log("Server start 😀, on port 3333!"));
