fc8ce2d739501455d2acd7e131075b35bc4ff4dd
Mário César Silva <mcgato33@hotmail.com>
2022-01-04 21:23:58 -0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
   if(err instanceof Error) {
     return response.status(400).json({
       message: err.message
-    })
+    });
   }
 
   return response.status(500).json({
