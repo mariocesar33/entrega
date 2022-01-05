c8b5211a98a3d761a0a347c931900ca15890c7de
Mário César Silva <mcgato33@hotmail.com>
2022-01-05 09:10:32 -0100

    Create CreateDeliverymanUseCase.ts

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,38 @@
+import { hash } from "bcrypt";
+
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+  async execute({username, password}: ICreateDeliveryman) {
+    // Validar se o deliveryman existe
+    const deliverymanExist = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        }
+      }
+    });
+
+    if (deliverymanExist) {
+      throw new Error("Deliveryman already exists!");
+    }
+
+    // Criptografar a palavra passe
+    const hashPassaword = await hash(password, 10);
+
+    // Salvar o deliveryman no banco de dados
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashPassaword,
+      }
+    });
+
+    return deliveryman;
+  }
+}
