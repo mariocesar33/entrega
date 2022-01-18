8283d01d6801455439b47f889dec4bf663059c4d
Mário César Silva <mcgato33@hotmail.com>
2022-01-18 11:32:57 -0100

    create route /deliveryman/authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
@@ -8,9 +9,11 @@ const routes = Router();
 
 const createClientController =  new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 const createDeliverymentController  = new CreateDeliverymanController();
 
-routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
 
 routes.post("/client/", createClientController.handle);
 routes.post("/deliveryment", createDeliverymentController.handle);
