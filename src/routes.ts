3d6b2014837f0a91f414cb4294bab72c2af3e9e2
Mário César Silva <mcgato33@hotmail.com>
2022-01-21 11:58:14 -0100

    criando rota de delevery

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { AuthenticateClientController } from "./modules/account/authenticateClie
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+
 
 const routes = Router();
 
@@ -11,11 +13,13 @@ const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 const createDeliverymentController = new CreateDeliverymanController();
+const deliveryController = new CreateDeliveryController();
 
 routes.post("/client/authenticate", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
 
 routes.post("/client/", createClientController.handle);
 routes.post("/deliveryment", createDeliverymentController.handle);
+routes.post("/delevery", deliveryController.handle);
 
 export { routes };
