3524d54c40ea043662e2364b47367dd2d5e609db
Mário César Silva <mcgato33@hotmail.com>
2022-01-24 10:29:50 -0100

    Create ensureAuthenticateClient.ts

---
+++ src/middlewares/ensureAuthenticateClient.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+export async function ensureAuthenticateClient(
+  request: Request, 
+  response: Response, 
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  // se não tiver nada dentro do authHeader, passar uma mensagem de erro!
+  if(!authHeader) {
+    return response.status(401).json({
+      message: "Token missing!",
+    });
+  }
+
+  // Se tiver alguma coisa preenchida, primeiro preciso pegar o que esta dentro do token
+  // a estrutura que vai vir é 1) Bearer e 2) bdjghfg65-54545
+  // mas Bearer não importa o que importa é token-> bdjghfg65-54545
+  // Para pegar só token fazemos o seguinte:
+  const [, token ] = authHeader.split(" ");
+  // [0] -> Bearer
+  // [1] -> bdjghfg65-54545
+
+  // Agora vamos validar se é um token valido
+  try {
+    const { sub } = verify(token, "1c0c5c598fd0f946bf8fd82183e1a0b0");
+
+    console.log(sub);
+    return next();
+  } catch(err) {
+    return response.status(401).json({
+      message: "Invalid token!",
+    });
+  }
+}
