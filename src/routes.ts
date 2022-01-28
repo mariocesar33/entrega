42cab05104424dc78fe3273a9f6175a5f8e75949
Mário César Silva <mcgato33@hotmail.com>
2022-01-28 09:39:11 -0100

    criando a rota "/delevery/available"

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { AuthenticateDeliverymanController } from "./modules/account/authenticat
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
 
 import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
 
@@ -16,6 +17,7 @@ const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 const createDeliverymentController = new CreateDeliverymanController();
 const deliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post("/client/authenticate", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
@@ -24,4 +26,5 @@ routes.post("/client/", createClientController.handle);
 routes.post("/deliveryment", createDeliverymentController.handle);
 routes.post("/delevery", ensureAuthenticateClient, deliveryController.handle);
 
+routes.get("/delevery/available", findAllAvailableController.handle);
 export { routes };
