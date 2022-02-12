16c4917054bec2860f897203276f289a24eb0fa3
Mário César Silva <mcgato33@hotmail.com>
2022-02-12 12:50:55 -0100

    Create FindAllDeliveriesDeliverymanController.ts

---
+++ src/modules/deliveryman/useCases/FindAllDeliveries/FindAllDeliveriesDeliverymanController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+import { FindAllDeliveriesDeliverymanUseCase } from "./FindAllDeliveriesDeliverymanUseCase";
+
+export class FindAllDeliveriesDeliverymanController {
+  async handle(request: Request, response: Response) {
+    const { id_deliveryman } = request.body;
+
+    const findAllDeliveriesDeliverymanCaseUse = new FindAllDeliveriesDeliverymanUseCase();
+    
+    const result = findAllDeliveriesDeliverymanCaseUse.execute(id_deliveryman);
+
+    return response.status(201).json(result);
+  }
+}
